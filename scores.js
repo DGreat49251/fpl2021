5edf466a5430f2fd89c08b4b43b9b11bdb2a9976
Dipak Ghosh <dgreat49251@gmail.com>
2021-09-19 23:37:31 +0530

    LEG 2 MATCH 2

--- scores.js
+++ scores.js
@@ -57,16 +57,16 @@ place(kanti, '#sb', 0);
 place(priyanshu, '#pm', 0);
 place(divyank, '#dr', 0);
 place(deepta, '#dkd', 0);
-var anik2 = [20]
-var dipak2 = [25]
-var tushar2 = [11]
-var arpan2 = [7]
+var anik2 = [20, 9, ]
+var dipak2 = [25, 12]
+var tushar2 = [11, 3]
+var arpan2 = [7, 2]
 var faizan2 = []
-var harsh2 = [6]
-var kanti2 = [0]
-var priyanshu2 = [4]
-var divyank2 = [14]
-var deepta2 = [0]
+var harsh2 = [6, 0]
+var kanti2 = [0,22]
+var priyanshu2 = [4, 0]
+var divyank2 = [14, 6]
+var deepta2 = [0, 17]
 var anikSum2 = sum(anik2);
 var dipakSum2 = sum(dipak2);
 var tusharSum2 = sum(tushar2);
