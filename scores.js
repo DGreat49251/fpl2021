35275f2fd2f058ef0c0a8bb3e9a8ab548453ebfc
Dipak Ghosh <dgreat49251@gmail.com>
2021-09-24 00:11:31 +0530

    KKR WIN UPDATE

--- scores.js
+++ scores.js
@@ -57,13 +57,13 @@ place(kanti, '#sb', 0);
 place(priyanshu, '#pm', 0);
 place(divyank, '#dr', 0);
 place(deepta, '#dkd', 0);
-var anik2 = [20, 9, 15, 11, 10]
-var dipak2 = [25, 12, 20, 22, 18]
-var tushar2 = [11, 3, 5, 10, 7]
-var arpan2 = [7, 2, 0, 6, 8]
-var kanti2 = [0, 22, 6, 16, 21]
+var anik2 = [20, 9, 15, 11, 10, 3]
+var dipak2 = [25, 12, 20, 22, 18, 6]
+var tushar2 = [11, 3, 5, 10, 7, 14]
+var arpan2 = [7, 2, 0, 6, 8, 20]
+var kanti2 = [0, 22, 6, 16, 21, 6]
 var divyank2 = [14, 6, 0, -14, -14]
-var deepta2 = [0, 17, 12, 10, 13]
+var deepta2 = [0, 17, 12, 10, 13, 9]
 var harsh2 =[]
 var priyanshu2 = []
 var faizan2 =[]
